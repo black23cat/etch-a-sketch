5f6dbcb05bb1dac50a5589deb40a792fd2f67a02
Prayogi <prayogi.pangestu1299@gmail.com>
2024-06-20 15:33:37 +0700

    Add function that create grid and generate color when mouse hovering divs

--- script.js
+++ script.js
@@ -2,19 +2,26 @@ const container = document.querySelector('.container');
 const btnContainer = document.querySelector('.button-container');
 const gridContainer = document.querySelector('.grid-container');
 
-for(let i = 0; i < 16; i++){
-  for(let j = 0; j < 16; j++){
-    const div = document.createElement('div');
-    div.style.width = `${(600/16)}px`;
-    div.style.height = `${(400/16)}px`;
-    div.classList.add('grid');
-    gridContainer.appendChild(div);
+function defaultColor(){
+  const gridDivs = document.querySelectorAll('.grid-container div');
+  gridDivs.forEach((element)=>{
+      element.addEventListener('mouseover', function(e) {
+        e.target.style.backgroundColor = 'limegreen';
+    });
+  })
+}
+
+function createGrid(gridSize=16){
+  for(let i = 0; i < gridSize; i++){
+    for(let j = 0; j < gridSize; j++){
+      const div = document.createElement('div');
+      div.style.width = `${(600/gridSize)}px`;
+      div.style.height = `${(400/gridSize)}px`;
+      div.classList.add('grid');
+      gridContainer.appendChild(div);
+    }
   }
 }
 
-const gridDivs = document.querySelectorAll('.grid-container div');
-gridDivs.forEach((element)=>{
-    element.addEventListener('mouseover', function(e) {
-      e.target.style.backgroundColor = 'limegreen';
-    });
-})
+createGrid();
+defaultColor();
